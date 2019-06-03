bdb0be2ca10b0507aab584e0f885dc13e3458c83
李人杰 <li.ren.jie@navercorp.com>
2019-06-03 17:32:01 +0800

    setState使用，以及多次使用的现象

---
+++ src/003setState/001index.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+/**
+ * 解决this指针的三种方法
+ * 1. this.add.bind(this) 把add方法里面的this指针绑定为组件实例
+ * 2. 使用匿名函数
+ * 3. 类的属性
+ */
+class StateTest extends React.Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      number: 1,
+    };
+  }
+
+  //强制更新，不管状态和属性修改没有，都会强制刷新界面
+  //直接修改state, this.state.num=123
+  //然后直接this.forceUpdate();
+
+  add = () => {
+    this.setState({number:this.state.number+1});
+    console.log('state=',this.state);
+    this.setState({number:this.state.number+1});
+    console.log('state=',this.state);
+
+    this.setState((state)=>({number:state.number+1}),()=>{
+      console.log('state=',this.state)
+    });
+    this.setState((state)=>({number:state.number+1}),()=>{
+      console.log('state=',this.state)
+    });
+    this.setState((state)=>({number:state.number+1}),()=>{
+      console.log('state=',this.state)
+    });
+  };
+
+
+  render() {
+    return <>
+      <p>{ this.state.number }</p>
+      <button onClick={this.add}>+</button>
+    </>
+  }
+}
+
+export default StateTest;
--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import Test1 from './jsx/index';
 // import Test2 from './jsx/ifelse.js';
-import Test3 from './002simpleRender/test.js';
+// import Test3 from './002simpleRender/test.js';
+import Test4 from './003setState/001index';
 
 
 // ReactDOM.render(<Test1/> , document.getElementById('root'));
 // ReactDOM.render(<Test2/> , document.getElementById('root'));
+ReactDOM.render(<Test4/> , document.getElementById('root'));
 
