cb6efc18154db1765d86961720549dd4c3434091
李人杰 <li.ren.jie@navercorp.com>
2019-06-10 13:23:51 +0800

    综合注释setState的问题

--- src/003setState/001index.js
+++ src/003setState/001index.js
@@ -19,24 +19,44 @@ class StateTest extends React.Component{
   //直接修改state, this.state.num=123
   //然后直接this.forceUpdate();
   add = () => {
+    // 对象也是存起来，对象是立即遍历存起来的数组，合并对象进this.state
+    this.setState({number:this.state.number+3});
+    // console.log('state=',this.state);
+    this.setState({number:this.state.number+2});
+    // console.log('state=',this.state);
+
+    // 函数也是存起来，第一个函数返回的值，是个partcialState部分的state，
+    // 应该也是立即合入this.state
+    // 只不过{ number: state.number+1 }是这样的，state是闭包保存的。
+    // 所以最后一个行又加一个this.setState({number:this.state.number+1})后，值又被覆盖成2了
+    // { number: this.state.number+1 }因为this.state是上一次被覆盖后的值
+    this.setState((state)=> {
+      console.log(state);
+      return {number:state.number+1};
+    },()=>{
+      console.log('第二个回调函数state=',this.state)
+    });
+    this.setState((state)=>{
+      console.log(state);
+      return {number:state.number+1};
+    },()=>{
+      console.log('第二个回调函数state=',this.state)
+    });
+    this.setState((state)=>{
+      console.log(state);
+      return {number:state.number+1};
+    },()=>{
+      console.log('第二个回调函数state=',this.state)
+    });
+
+    // 在下面加一行this.setState({number:this.state.number+1});
+    // render出的结果变成了2，合并state的时候，又给搞回去了。
+    // 感觉好像函数方式使用，state是接连的，但是没有被何如this.state的
+    // 直到空的回调函数执行完了才会合入this.state
+
     this.setState({number:this.state.number+1});
-    console.log('state=',this.state);
-    this.setState({number:this.state.number+1});
-    console.log('state=',this.state);
-
-    this.setState((state)=>({number:state.number+1}),()=>{
-      console.log('state=',this.state)
-    });
-
-    this.setState((state)=>({number:state.number+1}),()=>{
-      console.log('state=',this.state)
-    });
-
-    this.setState((state)=>({number:state.number+1}),()=>{
-      console.log('state=',this.state)
-    });
+    // console.log('state=',this.state);
   };
-
 
   render() {
     return <>
