7620bec350e57b410a2e3450da5f750b9af39c4a
李人杰 <li.ren.jie@navercorp.com>
2019-06-11 14:01:29 +0800

    属性校验

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import Test1 from './jsx/index';
-// import Test2 from './jsx/ifelse.js';
-// import Test3 from './002simpleRender/test.js';
-// import Test4 from './003setState/001index';
-// import Test5 from './004lifeCycle/001old.js';
-// import Test6 from './004lifeCycle/002new-snapshot.js';
-import Test7 from './004lifeCycle/003derivedState.js';
+// import Test1 from './jsx/ifelse.js';
+// import Test1 from './002simpleRender/test.js';
+// import Test1 from './003setState/001index';
+// import Test1 from './004lifeCycle/001old.js';
+// import Test1 from './004lifeCycle/002new-snapshot.js';
+import Test1 from './007propTypes/index.js';
 
 
-// ReactDOM.render(<Test1/> , document.getElementById('root'));
-ReactDOM.render(<Test7/> , document.getElementById('root'));
+ReactDOM.render(<Test1/> , document.getElementById('root'));
 
